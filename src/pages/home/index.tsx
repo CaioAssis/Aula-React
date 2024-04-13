6368e492c61975a0124c6742e7d1df47cffe6553
CaioAssis <caio.ab.assis@gmail.com>
2024-04-12 21:02:35 -0300

    cria index atividade2

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom"
-
+import { Link, useNavigate } from "react-router-dom"
+//useNavigate -> exemplo professor
 
 function Home(){
+    const navigate = useNavigate()
     return(
         <>
-            <h1>Home</h1>
+            <h1 onClick={() => {
+                navigate('/sobre') }}>Home</h1>
             <Link to='/sobre'>Ir para Sobre</Link>
         </>
     )
