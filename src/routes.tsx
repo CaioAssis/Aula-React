b7b8d5d6834404ebc72d0724580947f48277be28
CaioAssis <caio.ab.assis@gmail.com>
2024-04-26 19:19:33 -0300

    modifica routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,8 @@ import Tasks from './pages/tasks'
 import Formulario from './pages/form'
 
 const routes = createBrowserRouter([
-  {path: '/home', element: <Home />, errorElement: <NotFound/>}, // errorElement só precisa estar na raiz
+  {path: '/', element: <Home />, errorElement: <NotFound/>},
+  {path: '/home', element: <Home />}, // errorElement só precisa estar na raiz
   {path: '/sobre', element:<Sobre />},
   {path: '/formulario', element:<Formulario />},
   {path: '/tarefa', element:<Tasks />}
